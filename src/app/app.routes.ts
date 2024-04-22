c57789643001230b3d68adef2d3f3caf2eea8bfb
Shashwat Gupta <ShashwatGupta@bdo.in>
2024-04-22 17:15:33 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { PracticeComponent } from './practice/practice.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [ { path: '', component: PracticeComponent },
+{ path: 'first', component: AppComponent },];
+
---
+++ src/app/practice/practice.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-practice',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './practice.component.html',
+  styleUrl: './practice.component.css'
+})
+export class PracticeComponent {
+
+}
