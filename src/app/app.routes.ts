b238a203be6b25391088abcac0fb796e53af895e
Shashwat Gupta <ShashwatGupta@bdo.in>
2024-06-19 17:52:55 +0530

    changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,9 @@ import { PracticeComponent } from './table/table.component';
 import { AppComponent } from './app.component';
 import { BdoMalasiaDahboardComponent } from './bdo-malasia-dahboard/bdo-malasia-dahboard.component';
 import { LayoutComponent } from './layout/layout.component';
+import { Layout1Component } from './layout1/layout1.component';
 
 export const routes: Routes = [
-  { path: '', component: LayoutComponent },
+  { path: '', component: Layout1Component },
   { path: 'first', component: AppComponent },
 ];
---
+++ src/app/layout1/layout1.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout1',
+  standalone: true,
+  imports: [],
+  templateUrl: './layout1.component.html',
+  styleUrl: './layout1.component.css'
+})
+export class Layout1Component {
+
+}
